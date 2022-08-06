3b3b150d3e3547582959fd9e890047be5f6d5d46
Sergiy Martynov <martins.codespace.kh@gmail.com>
2022-08-06 14:15:44 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = "";
+  n = Math.abs(n) + '';
+  for (let i = 0; i < n.length; ++i)
+  {
+    result = result + n[n.length - i - 1];
+  }
+  return +result;
 }
